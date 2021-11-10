42853f75b631318e11fc74413d4e9d30b4fc7cc2
Александр <gosu_1994@mail.ru>
2021-11-10 09:59:28 +0300

    small refactoring of contacts test

---
+++ src/__fixtures__/users.js
@@ -0,0 +1,56 @@
+export const users = [{
+  gender: "male",
+  name: {
+    title: "Mr",
+    first: "Luis",
+    last: "Lewis"
+  },
+  location: {
+    street: {
+      number: 7284,
+      name: "The Avenue"
+    },
+    city: "Donabate",
+    state: "Westmeath",
+    country: "Ireland",
+    postcode: 63349,
+    coordinates: {
+      latitude: "21.1658",
+      longitude: "-168.1963"
+    },
+    timezone: {
+      offset: "+5:30",
+      description: "Bombay, Calcutta, Madras, New Delhi"
+    }
+  },
+  email: "luis.lewis@example.com",
+  login: {
+    uuid: "a8bbd4e9-69db-4cd0-a03a-8558d4a06918",
+    username: "brownmouse587",
+    password: "1025",
+    salt: "yJZTUZcx",
+    md5: "ed20a44eb4b8856e13948b6f25993b23",
+    sha1: "ec99c9cf9fe3854e85e12d00d26a5892eb7c6155",
+    sha256: "30d959ea7aee1b5447bc8387e0ff12f6e04326f939db534d01ef158446c6b2cb"
+  },
+  dob: {
+    date: "1962-02-09T12:04:33.266Z",
+    age: 59
+  },
+  registered: {
+    date: "2007-12-25T08:14:58.453Z",
+    age: 14
+  },
+  phone: "061-360-4605",
+  cell: "081-527-4568",
+  id: {
+    name: "PPS",
+    value: "3869810T"
+  },
+  picture: {
+    large: "https://randomuser.me/api/portraits/men/52.jpg",
+    medium: "https://randomuser.me/api/portraits/med/men/52.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/men/52.jpg"
+  },
+  nat: "IE"
+}]
--- src/__tests__/contacts.test.js
+++ src/__tests__/contacts.test.js
@@ -2,93 +2,25 @@ import React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import {rest} from 'msw'
 import Contacts from "../pages/Contacts";
-import {setupServer} from "msw/node";
 import {theme} from "../App";
 import {ThemeProvider} from "@mui/material";
-
-const users = [{
-  gender: "male",
-  name: {
-    title: "Mr",
-    first: "Luis",
-    last: "Lewis"
-  },
-  location: {
-    street: {
-      number: 7284,
-      name: "The Avenue"
-    },
-    city: "Donabate",
-    state: "Westmeath",
-    country: "Ireland",
-    postcode: 63349,
-    coordinates: {
-      latitude: "21.1658",
-      longitude: "-168.1963"
-    },
-    timezone: {
-      offset: "+5:30",
-      description: "Bombay, Calcutta, Madras, New Delhi"
-    }
-  },
-  email: "luis.lewis@example.com",
-  login: {
-    uuid: "a8bbd4e9-69db-4cd0-a03a-8558d4a06918",
-    username: "brownmouse587",
-    password: "1025",
-    salt: "yJZTUZcx",
-    md5: "ed20a44eb4b8856e13948b6f25993b23",
-    sha1: "ec99c9cf9fe3854e85e12d00d26a5892eb7c6155",
-    sha256: "30d959ea7aee1b5447bc8387e0ff12f6e04326f939db534d01ef158446c6b2cb"
-  },
-  dob: {
-    date: "1962-02-09T12:04:33.266Z",
-    age: 59
-  },
-  registered: {
-    date: "2007-12-25T08:14:58.453Z",
-    age: 14
-  },
-  phone: "061-360-4605",
-  cell: "081-527-4568",
-  id: {
-    name: "PPS",
-    value: "3869810T"
-  },
-  picture: {
-    large: "https://randomuser.me/api/portraits/men/52.jpg",
-    medium: "https://randomuser.me/api/portraits/med/men/52.jpg",
-    thumbnail: "https://randomuser.me/api/portraits/thumb/men/52.jpg"
-  },
-  nat: "IE"
-}]
-
-const handlers = [
-  rest.get('https://randomuser.me/api/?results=10', (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        results: users,
-      }),
-    )
-  }),
-]
-
-const server = setupServer(...handlers)
+import {server} from "../serverTests";
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
-const ComponentsWithTheme = () => {
-  return <ThemeProvider theme={theme}>
-    <Contacts/>
-  </ThemeProvider>
-}
-
 describe('contacts get data', () => {
+  const ComponentsWithTheme = () => {
+    return (
+      <ThemeProvider theme={theme}>
+        <Contacts/>
+      </ThemeProvider>
+    )
+  }
+
   test('loading', async () => {
-    render(<ComponentsWithTheme />)
+    render(<ComponentsWithTheme/>)
 
     const loader = screen.getByTestId('contacts-loader')
 
@@ -97,7 +29,7 @@ describe('contacts get data', () => {
   })
 
   test('success', async () => {
-    render(<ComponentsWithTheme />)
+    render(<ComponentsWithTheme/>)
     const loader = screen.getByTestId('contacts-loader')
 
     await waitForElementToBeRemoved(loader)
@@ -118,7 +50,7 @@ describe('contacts get data', () => {
       }),
     )
 
-    render(<ComponentsWithTheme />)
+    render(<ComponentsWithTheme/>)
     const error = await screen.findByText('...something went wrong')
     expect(error).toBeInTheDocument()
   })
